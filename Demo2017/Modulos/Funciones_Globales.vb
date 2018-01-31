650c54d5a932d57170ded77844ba915c4e45df77
alejandro <alejandroheredia95@gmail.com>
2018-01-31 16:14:51 -0600

    Funciones agregadas. Se agrego la función de encriptacion de datos y desencriptación. Aun no se han puesto en uso en las demas funciones qu trabajan con archivos.

--- Demo2017/Modulos/Funciones_Globales.vb
+++ Demo2017/Modulos/Funciones_Globales.vb
@@ -73,4 +73,44 @@ Module Funciones_Globales
             Return True
         End If
     End Function
+
+    Private Function EncryptFile(ByVal filepath As String, ByVal key As String)
+        Dim plainContent As Byte() = File.ReadAllBytes(filepath)
+        Dim DES As New DESCryptoServiceProvider()
+        Using (DES)
+            DES.IV = Encoding.UTF8.GetBytes(key)
+            DES.Key = Encoding.UTF8.GetBytes(key)
+            DES.Mode = CipherMode.CBC
+            DES.Padding = PaddingMode.PKCS7
+
+            Dim memStream = New MemoryStream
+            Using (memStream)
+                Dim cryptoStream As CryptoStream = New CryptoStream(memStream, DES.CreateEncryptor(), CryptoStreamMode.Write)
+
+                cryptoStream.Write(plainContent, 0, plainContent.Length)
+                cryptoStream.FlushFinalBlock()
+                File.WriteAllBytes(filepath, memStream.ToArray())
+            End Using
+
+        End Using
+    End Function
+    Private Function DecryptFile(ByVal filepath As String, ByVal key As String)
+        Dim encrypted As Byte() = File.ReadAllBytes(filepath)
+        Dim DES As New DESCryptoServiceProvider()
+        Using (DES)
+            DES.IV = Encoding.UTF8.GetBytes(key)
+            DES.Key = Encoding.UTF8.GetBytes(key)
+            DES.Mode = CipherMode.CBC
+            DES.Padding = PaddingMode.PKCS7
+
+            Dim memStream = New MemoryStream
+            Using (memStream)
+                Dim cryptoStream As CryptoStream = New CryptoStream(memStream, DES.CreateDecryptor(), CryptoStreamMode.Write)
+
+                cryptoStream.Write(encrypted, 0, encrypted.Length)
+                cryptoStream.FlushFinalBlock()
+                File.WriteAllBytes(filepath, memStream.ToArray())
+            End Using
+        End Using
+    End Function
 End Module
